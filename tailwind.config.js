4927d2bcb47e9a22e378948e33c2facefee6bb58
Dnt1009 <135596064+Dnt1009@users.noreply.github.com>
2025-07-04 01:46:27 +0700

    Add notification page, campaignlist and registerdao

---
+++ tailwind.config.js
@@ -0,0 +1,61 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,html,mdx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        global: {
+          background1: "var(--global-bg-1)",
+          background2: "var(--global-bg-2)",
+          background3: "var(--global-bg-3)",
+          background4: "var(--global-bg-4)",
+          background5: "var(--global-bg-5)",
+          background6: "var(--global-bg-6)",
+          background7: "var(--global-bg-7)",
+          text1: "var(--global-text-1)",
+          text2: "var(--global-text-2)",
+          text3: "var(--global-text-3)",
+          text4: "var(--global-text-4)",
+          text5: "var(--global-text-5)",
+          text6: "var(--global-text-6)",
+          text7: "var(--global-text-7)",
+          text8: "var(--global-text-8)",
+          text9: "var(--global-text-9)",
+          text10: "var(--global-text-10)",
+          text11: "var(--global-text-11)",
+          text12: "var(--global-text-12)",
+          text13: "var(--global-text-13)",
+          text14: "var(--global-text-14)"
+        },
+        searchview: {
+          background1: "var(--searchview-bg-1)",
+          text1: "var(--searchview-text-1)"
+        },
+        button: {
+          background1: "var(--button-bg-1)",
+          background2: "var(--button-bg-2)",
+          background3: "var(--button-bg-3)",
+          background4: "var(--button-bg-4)",
+          text1: "var(--button-text-1)"
+        },
+        slider: {
+          text1: "var(--slider-text-1)"
+        },
+        pagerindicator: {
+          background1: "var(--pagerindicator-bg-1)"
+        },
+        footer: {
+          background1: "var(--footer-bg-1)",
+          text1: "var(--footer-text-1)"
+        },
+        edittext: {
+          text1: "var(--edittext-text-1)"
+        },
+        checkbox: {
+          text1: "var(--checkbox-text-1)"
+        }
+      }
+    }
+  },
+  plugins: []
+};
